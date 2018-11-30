beafed7eee29d7e1e95e6fd3d05e7be2848d9091
Rahul Anand <rahul.anand@springernature.com>
2018-11-30 14:37:41 +0530

    changing configurations to make redis role idempotent.

--- test/integration/sentinel/serverspec/sentinel_spec.rb
+++ test/integration/sentinel/serverspec/sentinel_spec.rb
@@ -1,24 +1,43 @@
 require 'spec_helper'
 
 describe 'Redis' do
-  describe service('sentinel_26379') do
+  describe service('redis_6379') do
     it { should be_enabled }
     it { should be_running }
   end
 
-  describe port(26379) do
+  describe port(6379) do
     it { should be_listening.on('0.0.0.0').with('tcp') }
   end
 
-  describe file('/etc/redis/sentinel_26379.conf') do
+  describe file('/etc/redis/6379.conf') do
     it { should be_file }
     it { should be_owned_by 'redis' }
-    its(:content) { should match /port 26379/ }
+    its(:content) { should include "include /etc/redis/6379.include.conf" }
   end
 
-  describe file('/var/run/redis/sentinel_26379.pid') do
+  describe file('/etc/redis/6379.include.conf') do
+    it { should be_file }
+    it { should be_owned_by 'redis' }
+    its(:content) { should match /port 6379/ }
+  end
+
+  describe file('/var/run/redis/6379.pid') do
     it { should be_file }
     it { should be_owned_by 'redis' }
     its(:size) { should > 0 }
+  end
+
+  describe file('/proc/sys/vm/overcommit_memory') do
+    it { should be_file }
+    it { should contain '1' }
+  end
+
+  describe file('/usr/bin/redis-server') do
+    it { should be_symlink }
+  end
+
+  describe file('/usr/bin/redis-cli') do
+    it { should be_symlink }
   end
 end
